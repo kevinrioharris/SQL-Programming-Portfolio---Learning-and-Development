5cd2cb8032b41388f316c5368b335a7e2a301441
Kevin Rio Harristyando <125848410+kevinrioharris@users.noreply.github.com>
2023-07-21 17:59:24 +0700

    Create NameOfEmployees.sql

---
+++ HackerRank/Basic Select/NameOfEmployees.sql
@@ -0,0 +1,7 @@
+#Problem: Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+#Language: MySQL
+
+SELECT NAME
+FROM EMPLOYEE
+ORDER BY NAME;
