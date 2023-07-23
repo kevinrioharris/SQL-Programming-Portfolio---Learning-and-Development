39e990d77a4c4ca3e3f1dc57c57ba29a804f6917
Kevin Rio Harristyando <125848410+kevinrioharris@users.noreply.github.com>
2023-07-23 20:59:23 +0700

    Update Occupations.sql
    Change comment

--- HackerRank/Advanced Select/Occupations.sql
+++ HackerRank/Advanced Select/Occupations.sql
@@ -1,4 +1,6 @@
-#Problem: Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+#Problem: Pivot the Occupation column in OCCUPATIONS so that each Name 
+#is sorted alphabetically and displayed underneath its corresponding Occupation. 
+#The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
 
 #MySQL
 
