f24f0c238941fe7262f2b23766b74df82a4552c8
Kevin Rio Harristyando <125848410+kevinrioharris@users.noreply.github.com>
2023-07-26 13:59:42 +0700

    Create Contest Leaderboard.sql

---
+++ HackerRank/Basic Join/Contest Leaderboard.sql
@@ -0,0 +1,12 @@
+#MySQL
+
+SELECT H.hacker_id, H.name, SUM(M.max_score) AS total_score
+FROM (
+    SELECT S.hacker_id, S.challenge_id, max(S.score) as max_score
+    FROM Submissions AS S GROUP BY S.hacker_id, S.challenge_id
+    ) AS M
+INNER JOIN Hackers AS H 
+ON H.hacker_id = M.hacker_id
+GROUP BY H.hacker_id, H.name
+HAVING total_score > 0
+ORDER BY total_score DESC, H.hacker_id ASC;
